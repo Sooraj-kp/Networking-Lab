42d1295746db00491af9f3a8a2937a3cb158c627
Sooraj-kp <soorajkp2332@gmail.com>
2025-03-06 10:47:44 +0530

    Add files via upload

---
+++ March/prgrms_files/date_and_time.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+current_date_time=$(date)
+username=$(whoami)
+current_directory=$(pwd)
+echo "Current Date and Time :$current_date_time"
+echo "Username : $username"
+echo "Current Working directory :$current_directory"
+
