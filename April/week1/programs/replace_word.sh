e15e6d62f2e42b7bf125293da02d424053173efe
Sooraj-kp <soorajkp2332@gmail.com>
2025-04-03 11:53:01 +0530

    Add files via upload

---
+++ April/week1/programs/replace_word.sh
@@ -0,0 +1,13 @@
+#!/bib/bash
+if [ $# -ne 3 ]; then
+	echo " Usage :$0 <file> <old_word> <new_word>"
+	exit 1
+fi
+
+file=$1
+old_word=$2
+new_word=$3
+
+sed -i "s/$old_word/$new_word/g" "$file"
+
+echo "All occurences of '$old_word' have been replaced with '$new_word' in $file."
